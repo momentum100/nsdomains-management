55ac7e00ac17bc3dd68d75b8858d22f47a6eff26
General M <51231797+momentum100@users.noreply.github.com>
2025-02-18 14:32:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware(['auth'])->group(function () {
 });
 
 // Authentication routes without registration
-Auth::routes(['register' => false]);
+Auth::routes(['register' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
