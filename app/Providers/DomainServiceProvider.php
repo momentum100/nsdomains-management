a198c2953b93787483ec6fce30ed43838fc6b0c3
General M <51231797+momentum100@users.noreply.github.com>
2025-03-14 13:12:57 +0200

    Update DomainServiceProvider.php

--- app/Providers/DomainServiceProvider.php
+++ app/Providers/DomainServiceProvider.php
@@ -6,13 +6,25 @@ use Illuminate\Support\ServiceProvider;
 use App\Services\WhoisService;
 use App\Services\PricingService;
 use App\Services\DomainService;
+use App\Services\ProxyService;
+use Illuminate\Support\Facades\Log;
 
 class DomainServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // First register ProxyService (it has default parameters so can be instantiated without arguments)
+        $this->app->singleton(ProxyService::class, function ($app) {
+            Log::info("Registering ProxyService in container");
+            return new ProxyService();
+        });
+
+        // Then register WhoisService which depends on ProxyService
         $this->app->singleton(WhoisService::class, function ($app) {
-            return new WhoisService();
+            Log::info("Registering WhoisService in container");
+            // Get the ProxyService from the container
+            $proxyService = $app->make(ProxyService::class);
+            return new WhoisService($proxyService);
         });
 
         $this->app->singleton(PricingService::class, function ($app) {
